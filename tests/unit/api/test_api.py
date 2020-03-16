0fbcb019065c3d174436f2490390cc455acf9738
voidnologo <(none)>
2020-03-16 19:45:56 -0400

    Post test example

--- tests/unit/api/test_api.py
+++ tests/unit/api/test_api.py
@@ -16,13 +16,27 @@ class ApiTests(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
-    def test_article_get_returns_all_articles(self):
+    def test_get_request_for_articles_returns_all_articles(self):
         Article(article_title='Test1', article_url='/url/url').save()
         Article(article_title='Test2', article_url='/url/url').save()
         response = self.client.get('/api/articles')
         self.assertEqual(response.status_code,  200)
         expected = [
-            {'title': 'Test1', 'url': '/url/url'},
-            {'title': 'Test2', 'url': '/url/url'}
+            {'title': 'Test1', 'url': '/url/url', 'id': 1},
+            {'title': 'Test2', 'url': '/url/url', 'id': 2}
         ]
         self.assertEqual(response.json, expected)
+
+    def test_posting_article_creates_articles(self):
+        title = 'new article'
+        url = '/my/cool/url'
+        data = {
+            'article_title': title,
+            'article_url': url
+        }
+        response = self.client.post('/api/articles', json=data)
+        self.assertEqual(response.status_code,  200)
+        articles = Article.query.all()
+        self.assertEqual(len(articles), 1)
+        self.assertEqual(articles[0].article_title, title)
+        self.assertEqual(articles[0].article_url, url)
